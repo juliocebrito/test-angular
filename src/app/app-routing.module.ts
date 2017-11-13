bb6cb2aadbe0f113c45141e0e1c72e80b258c89f
Julio Brito <jucebridu@gmail.com>
2017-11-13 18:50:24 -0500

    fix error en app.routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
-import { IndexComponent } from './index/index.component';
 import { AuthComponent } from './auth/auth.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  // { path: '', component: IndexComponent },
   { path: '', component: AuthComponent },
   // { path: '', redirectTo: '/auth', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent }
